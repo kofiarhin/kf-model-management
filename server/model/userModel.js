a92f0c01c6151574ea96114cac7118c113147f22
kofi <kofiarhin69@gmail.com>
2024-03-16 19:12:49 +0000

    update to code

--- server/model/userModel.js
+++ server/model/userModel.js
@@ -18,6 +18,10 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    featured: {
+      type: Boolean,
+      default: false,
+    },
     userType: {
       type: String,
       required: true,
