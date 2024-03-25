f3e98f8b983d9e3f1018e339fece33734b384701
kofi <kofiarhin69@gmail.com>
2024-03-25 20:26:03 +0000

    updated to landing page

--- client/src/Components/Landing/Landing.jsx
+++ client/src/Components/Landing/Landing.jsx
@@ -5,7 +5,15 @@ import "./landing.styles.scss";
 const Landing = () => {
   return (
     <div id="landing">
-      <div className="overlay"></div>
+      <div className="overlay">
+        <div className="content-wrapper">
+          <h2>
+            Unleash Your Potential. Connect with Top Agencies. Find Your Next
+            Gig.
+          </h2>
+          <Link to="/register">Join Now</Link>
+        </div>
+      </div>
       <div className="img-container">
         <img src="/images/main.jpg" alt="" />
       </div>
